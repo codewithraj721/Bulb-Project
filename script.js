3c903e54b68b3978c7b169e27637de2646504b64
Chandra Prakash <161104110+codewithraj721@users.noreply.github.com>
2024-06-14 15:44:02 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+// Javascript is a Bulb PRATICAL IN a using javascript
+
+function bulbon(){
+    document.getElementById("img1").src="bulbon.png";
+}
+
+function bulboff(){
+    document.getElementById("img1").src="bulboff.png";
+}
+
+
+   
